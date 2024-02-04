83865d2b21fd8be1d47c239d13a8839ffeaf4ac0
Rohit V <vrohit.dev@gmail.com>
2024-02-04 20:30:51 +0530

    Removed an unneeded file

--- wow.py
+++
@@ -1,8 +0,0 @@
-from car_factory import CarFactory
-from datetime import datetime
-
-current_date = datetime.today().date()
-
-car = CarFactory.create_calliope(current_date, current_date.replace(year=current_date.year - 1), 30000, 0)
-
-print(car.needs_service())
